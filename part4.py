cbd2ab8d2e259f61d4dfffab2513a6bbe80beb09
Fatema ARSIWALA <62667976+Fatema-ARSI@users.noreply.github.com>
2023-01-25 11:41:59 +0100

    Add files via upload

---
+++ part4.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+
+#add an import to Hydralit
+from hydralit import HydraHeadApp
+
+#create a wrapper class
+class noncorrelated_etfs(HydraHeadApp):
+
+    #wrap all your code in this method and you should be done
+
+    def run(self):
+
+        ############################################ main code ###############################################
+
+        st.write(" ## Fatema's part  ")
